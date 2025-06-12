9430538d632fb338918c280ecf538b6201067552
vitor-carvalho-ddt <vitor.carvalho.ufu@gmail.com>
2025-06-12 16:35:56 -0300

    feat: adding previewer

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    { 
+    {
       'nvim-telescope/telescope-fzf-native.nvim',
       build = 'make',
       cond = function()
@@ -33,7 +33,7 @@ return {
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', function()
-      require('telescope.builtin').find_files { previewer = false }
+      require('telescope.builtin').find_files { previewer = true }
     end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
@@ -42,11 +42,11 @@ return {
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', function()
-      require('telescope.builtin').buffers { previewer = false }
+      require('telescope.builtin').buffers { previewer = true }
     end, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[ ] Live Grep current project' })
     vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzy Find current Buffer' })
-    
+
     vim.keymap.set('n', '<leader>s/', function()
       builtin.live_grep {
         grep_open_files = true,
