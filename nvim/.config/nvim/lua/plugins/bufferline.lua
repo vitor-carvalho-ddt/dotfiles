24a3d94e6397bdf1863fdf3ffff3265a6f1a9043
vitor-carvalho-ddt <vitor.carvalho.ufu@gmail.com>
2025-06-18 14:12:03 -0300

    feat: <leader>bD close other buffers

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -5,13 +5,20 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      mode = "buffers",
-      diagnostics = "nvim_lsp",
-      separator_style = "thin",
+      mode = 'buffers',
+      diagnostics = 'nvim_lsp',
+      separator_style = 'thin',
       show_buffer_close_icons = true,
       show_close_icon = false,
       color_icons = true,
       always_show_bufferline = true,
+    },
+  },
+  keys = {
+    {
+      '<leader>bD',
+      ':BufferLineCloseOthers<CR>',
+      desc = 'Close other buffers',
     },
   },
 }
