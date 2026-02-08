3de70e8a765d98d03e80936df609f0acee2c494d
vitor-carvalho-ddt <vitor.carvalho.ufu@gmail.com>
2026-02-08 15:31:37 -0300

    fix: treesitter and neogit config errors

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -16,24 +16,24 @@ return {
   config = function()
     local neogit = require 'neogit'
     neogit.setup {
-      -- Hides the hints at the top of the status buffer
-      disable_hint = false,
-      -- Disables changing the buffer highlights based on where the cursor is.
-      disable_context_highlighting = false,
-      -- Disables signs for sections/items/hunks
-      disable_signs = false,
-      -- Offer to force push when branches diverge
-      prompt_force_push = true,
-      -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
-      -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
-      -- normal mode.
-      disable_insert_on_commit = 'auto',
-      -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
-      -- events.
-      filewatcher = {
-        interval = 1000,
-        enabled = true,
-      },
+      -- -- Hides the hints at the top of the status buffer
+      -- disable_hint = false,
+      -- -- Disables changing the buffer highlights based on where the cursor is.
+      -- disable_context_highlighting = false,
+      -- -- Disables signs for sections/items/hunks
+      -- disable_signs = false,
+      -- -- Offer to force push when branches diverge
+      -- prompt_force_push = true,
+      -- -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
+      -- -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
+      -- -- normal mode.
+      -- disable_insert_on_commit = 'auto',
+      -- -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
+      -- -- events.
+      -- filewatcher = {
+      --   interval = 1000,
+      --   enabled = true,
+      -- },
       -- "ascii"   is the graph the git CLI generates
       -- "unicode" is the graph like https://github.com/rbong/vim-flog
       -- "kitty"   is the graph like https://github.com/isakbm/gitgraph.nvim - use https://github.com/rbong/flog-symbols if you don't use Kitty
@@ -43,13 +43,6 @@ return {
       log_date_format = nil,
       -- Show message with spinning animation when a git command is running.
       process_spinner = false,
-      -- Used to generate URL's for branch popup action "pull request".
-      git_services = {
-        ['github.com'] = 'https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1',
-        ['bitbucket.org'] = 'https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1',
-        ['gitlab.com'] = 'https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}',
-        ['azure.com'] = 'https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}',
-      },
 
       sections = {
         -- Reverting/Cherry Picking
